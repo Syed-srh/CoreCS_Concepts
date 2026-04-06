47c54c8bc83ea1ee7fe7b99bc280c45e57e4c1ba
Syed Rahil Hussain <syedrahilhussain13@gmail.com>
2026-04-06 20:30:08 +0530

    Define Musician, Athlete, and Person classes
    Add class definitions and method stubs for Musician, Athlete, and Person classes in Problem5.py.

--- Day4_Inheritance/ProblemsToSolve/Problem5.py
+++ Day4_Inheritance/ProblemsToSolve/Problem5.py
@@ -1,4 +1,20 @@
+# 5: Multi-Talent System (Multiple Inheritance)
+# Complete the Musician, Athlete, and Person classes.
 
+# Musician class:
+# Add below methods:
+# play_instrument() : Prints instrument playing message
+# Athlete class:
+# Add below methods:
+# play_sport() : Prints sport playing message
+# Person class:
+# The Person class inherits from both Musician and Athlete.
+
+# Features to be added to Person class
+# Add below attributes:
+# name
+# Add below methods:
+# show_talents() : Calls both parent methods
 
 class Musician:
     def __init__(self, instrument):
